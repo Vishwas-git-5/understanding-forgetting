5c961f2d852db06e12295dbf7ee7be731ca9ab1e
Vishwas-git-5 <93853639+Vishwas-git-5@users.noreply.github.com>
2024-06-24 20:06:40 +0530

    Update create_batch.py

--- icl_vs_if/create_batch.py
+++ icl_vs_if/create_batch.py
@@ -1,6 +1,5 @@
 from itertools import product
 import pandas as pd
-from datetime import datetime
 
 def concat_csvs(filenames, resulting_filename):
     location = '/content/'
@@ -20,22 +19,19 @@ files = []
 for (task, shot), lang, instr, prompt_template in product(task_list, lang_list, instr_list, prompt_template_list):
     files.append(f"{task}-{instr}-{prompt_template}-{lang}-{shot}shot.csv")
 
-curr_time = datetime.now()
-formatted_time = curr_time.strftime("%m-%d-%H-%M-%S")
-concatenated_filehandle = f"batch-{formatted_time}"
+concatenated_filehandle = "batch"
 
 concat_csvs(files, concatenated_filehandle)
 
-print(f'Generated {concatenated_filehandle}')
+print(f'Generated {concatenated_filehandle}.csv')
 
-complete_command = f"""
+complete_command = """
 import subprocess
 
-subprocess.run(["python3", "/content/understanding-forgetting/icl_vs_if/generate.py", "--model", "alpaca", "--batch", "{concatenated_filehandle}"])
+subprocess.run(["python3", "/content/understanding-forgetting/icl_vs_if/generate.py", "--model", "alpaca", "--batch", "batch"])
 """
 
 with open("/content/understanding-forgetting/batch_generate.py", "w") as f:
     f.write(complete_command)
 
 print("Generated batch_generate.py")
-
